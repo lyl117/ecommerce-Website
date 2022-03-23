da63a2d358f59ac91ea5fb480ed6deb850d79790
yurimlee <ylee981352@gmail.com>
2022-03-23 23:33:12 +0900

    [#2] create main page(slide,icon,button)

--- ECOMMERCE/script.js
+++ ECOMMERCE/script.js
@@ -1,8 +1,8 @@
-const wrapper = document.querySelector(".sliderWrapper")
-const menuItems = document.querySelectorAll(".menuItem")
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
 
 menuItems.forEach((item,index)=>{
-  item.addEventListener("click", ()=>{
-    console.log("you clicked!" + index);
+  item.addEventListener("click", () => {
+   wrapper.style.transform = `translateX(${-100 * index}vw)`;
   });
 });
